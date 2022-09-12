eaa2c55d18140798021881281920cc6efeee5f99
nguyenvietanh2412 <111826776+nguyenvietanh2412@users.noreply.github.com>
2022-09-12 14:52:05 +0700

    Quan ly hoc sinh

--- bai_3/Thisinh.h
+++ bai_3/Thisinh.h
@@ -1,1 +1,41 @@
+#pragma once
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Thisinh
+{
+protected:
+	string hoten, diachi, khoithi;
+	int mucuutien;
+public:
+	int TS;
+	char sbd[10];
+	virtual void input()
+	{
+		cin.ignore();
+		cout << "\nNhap so bao danh: "; cin.getline(sbd, 10);
+		cout << "\nNhap ho va ten thi sinh: "; getline(cin, hoten);
+		cout << "\nNhap dia chi: "; getline(cin, diachi);
+		cout << "\nNhap muc uu tien: "; cin >> mucuutien;
+		cin.ignore();
+
+	}
+	virtual void output()
+	{
+		cout << "\nSo bao danh: " << sbd;
+		cout << "\nHo va ten: " << hoten;
+		cout << "\nDia chi: " << diachi;
+		cout << "\nMuc uu tien: " << mucuutien;
+	}
+	int getTS()
+	{
+		return TS;
+	}
+	void setTS(int tl)
+	{
+		TS = tl;
+	}
+	~Thisinh() {}
+};
 
