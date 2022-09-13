0a9b7ae4c02d554d80a72b1776b0601a3f2c2ad5
nguyenvietanh2412 <111826776+nguyenvietanh2412@users.noreply.github.com>
2022-09-13 15:47:08 +0700

    Quan ly khu pho

--- bai_4/Person.h
+++ bai_4/Person.h
@@ -1,1 +1,38 @@
+#pragma once
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Person
+{
+private:
+	string name, job;
+	int age, id;
+public:
+	Person(){}
+	Person(string Name, string Job, int Age, int ID)
+	{
+		name = Name;
+		job = Job;
+		age = Age;
+		id = ID;
+	}
+	string getName()
+	{
+		return name;
+	}
+	string getJob()
+	{
+		return job;
+	}
+	int getAge()
+	{
+		return age;
+	}
+	int getID()
+	{
+		return id;
+	}
+	~Person(){}
+};
 
